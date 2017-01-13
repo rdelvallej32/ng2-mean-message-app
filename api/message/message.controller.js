a5f95504f2e0f441121ed226389a132d0566fb78
rdelvallej32 <rdelvallej@gmail.com>
2017-01-13 18:06:57 -0500

    Add controller actions for create and index

---
+++ api/message/message.controller.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import Message from './message.model';
+
+function validationError(res, statusCode) {
+  statusCode = statusCode || 422;
+  return function(err) {
+    return res.status(statusCode).json(err);
+  };
+}
+
+function handleError(res, statusCode) {
+  statusCode = statusCode || 500;
+  return function(err) {
+    return res.status(statusCode).send(err);
+  };
+}
+
+function respondWithResult(res, statusCode) {
+  statusCode = statusCode || 200;
+  return function(entity) {
+    return res.status(statusCode).json(entity);
+  };
+}
+
+export function index(req, res) {
+  Message.findAsync()
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+export function create(req, res) {
+  Message.createAsync(req.body)
+    .then(respondWithResult(res, 201))
+    .catch(handleError(res));
+}
