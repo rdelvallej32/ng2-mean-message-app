d0ed2d197f525c092031127ec7bbc29dd3a15459
rdelvallej32 <rdelvallej@gmail.com>
2017-01-15 12:59:57 -0500

    Import message service and OnInit to MsgLstCmpnt
    - OnInit interface implements the ngOnInit call to run on initialization
    - Use the message service's getMessages function to assign the
    local property messages array to the messages array in the service

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from './message.model';
+import { MessageService } from './message.service';
 
 @Component({
     selector: 'app-message-list',
@@ -10,14 +11,18 @@ import { Message } from './message.model';
         (editClicked)="editClicked(message, $event)">
       </app-message>
     </div>
-  `
+  `,
+    providers: [MessageService]
 })
 
-export class MessageListComponent {
-    messages: Message[] = [
-        new Message('Some Message', 'Bob'),
-        new Message('Patriots Suck', 'Rob')
-    ];
+export class MessageListComponent implements OnInit {
+    messages: Message[];
+
+    constructor(private messageService: MessageService) { }
+
+    ngOnInit() {
+        this.messages = this.messageService.getMessages();
+    }
 
     editClicked(message: Message, event: string) {
         message.content = event;
