470cd644589c7b4b5ddaf2776c8da7e29ad282c6
rdelvallej32 <rdelvallej@gmail.com>
2017-01-15 09:51:33 -0500

    Add panel for a message in appComponent
    - Add bootstrap classes for the panel
    - Add a message object for the app component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [`
+      .author {
+        display: inline-block;
+        font-style: italic;
+        width: 80%;
+        font-stize: 12px;
+      }
+      .config {
+        display: inline-block;
+        text-align: right;
+        font-stize: 12px;
+        width: 19%;
+      }
+      `]
 })
 export class AppComponent {
     content = 'hi';
+    message = {
+        content: 'Practicing some angular2, ahoy!',
+        author: 'Bob'
+    };
 }
