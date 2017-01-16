c6245d047fea971194680f4af295fa361151799a
rdelvallej32 <rdelvallej@gmail.com>
2017-01-16 17:48:51 -0500

    Add Reactive form elements to sign-in component
    - Use FormGroup, Validators, and FormControl modules from angular core
    to build out the form controls for the sign-in component

--- assets/app/auth/sign-in.component.ts
+++ assets/app/auth/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-sign-in',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
 })
 
 export class SignInComponent {
+    myForm: FormGroup;
 
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            email: new FormControl('', [
+                Validators.required,
+                Validators.pattern('/^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0 - 9]{1,3}.[0 - 9]{1,3}.[0 - 9]{1,3}.[0 - 9]{1,3}]) | (([a - zA - Z - 0 - 9] +.)+ [a - zA - Z]{2,}))$ / ')
+            ]),
+            password: new FormControl('', Validators.required),
+        });
+    }
+
+    onSubmit() {
+        console.log(this.myForm);
+    }
 }
