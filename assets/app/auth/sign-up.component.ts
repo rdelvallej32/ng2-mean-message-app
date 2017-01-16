c5f753760ab3134804287683324c6c925261ad08
rdelvallej32 <rdelvallej@gmail.com>
2017-01-16 17:44:26 -0500

    Add Reactive Form Elements to signup component
    - Use FormGroup, FormControl and Validators to build the form for the
    sign up component

--- assets/app/auth/sign-up.component.ts
+++ assets/app/auth/sign-up.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-sign-up',
     templateUrl: './sign-up.component.html'
 })
 
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+    myForm: FormGroup;
 
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            firstName: new FormControl('', Validators.required),
+            lastName: new FormControl('', Validators.required),
+            email: new FormControl('', [
+                Validators.required,
+                Validators.pattern('/^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0 - 9]{1,3}.[0 - 9]{1,3}.[0 - 9]{1,3}.[0 - 9]{1,3}]) | (([a - zA - Z - 0 - 9] +.)+ [a - zA - Z]{2,}))$ / ')
+            ]),
+            password: new FormControl('', Validators.required),
+        });
+    }
+
+    onSubmit() {
+        console.log(this.myForm);
+    }
 }
