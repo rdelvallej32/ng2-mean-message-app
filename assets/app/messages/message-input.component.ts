d759cda5618e47f0b16e83592d6033b1bf2af658
rdelvallej32 <rdelvallej@gmail.com>
2017-01-17 18:58:09 -0500

    Subscribe to edit message event
    - The message input component subscribes to the messageIsEdit
    event.
    - Add a message object and assign it the selected message that is
    returned from the subscription.

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MessageService } from './message.service';
 import { Message } from './message.model';
@@ -8,17 +8,25 @@ import { Message } from './message.model';
     templateUrl: './message-input.component.html'
 })
 
-export class MessageInputComponent {
-    constructor(private MessageService: MessageService) { }
+export class MessageInputComponent implements OnInit {
+    message: Message;
+
+    constructor(private messageService: MessageService) { }
+
+    ngOnInit() {
+        this.messageService.messageIsEdit.subscribe(
+            (message: Message) => this.message = message
+        );
+    }
 
     onSave(value: string) {
         const message = new Message(value, 'Rob');
-        this.MessageService.addMessage(message);
+        this.messageService.addMessage(message);
     }
 
     onSubmit(form: NgForm) {
         const message = new Message(form.value.content, 'Robby');
-        this.MessageService.addMessage(message)
+        this.messageService.addMessage(message)
             .subscribe(
             data => console.log(data),
             err => console.log(err)
