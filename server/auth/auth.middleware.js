1e38f454ed5d8382dcf867f3088f8678c3c27ead
rdelvallej32 <rdelvallej@gmail.com>
2017-01-23 21:36:54 -0500

    Add user to request header if token found
    - If a token is verified, then decode it and find the token of
    the correct User.
    
    - Add the user to the request header, and then move onto the next
    process for the controller action

--- server/auth/auth.middleware.js
+++ server/auth/auth.middleware.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import jwt from 'jsonwebtoken';
+import User from '../api/user/user.model';
 
 exports.isAuthenticated = (req, res, next) => {
   jwt.verify(req.query.token, 'secret', (err, decoded) => {
@@ -9,7 +10,17 @@ exports.isAuthenticated = (req, res, next) => {
         title: 'Not Authenticated',
         error: err
       });
+    } else {
+      const token = jwt.decode(req.query.token);
+      User.findById(token.entity._id).exec()
+        .then(user => {
+          if (!user) {
+            return res.status(401).end();
+          }
+          req.user = user;
+          next();
+        })
+        .catch(err => console.error(err));
     }
-    next();
   });
 };
