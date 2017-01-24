bf93b292394864c3cff17cf168a1954ba5b0a487
rdelvallej32 <rdelvallej@gmail.com>
2017-01-23 21:39:03 -0500

    Add middleware on removal event of a message to update User's messages

--- server/api/message/message.model.js
+++ server/api/message/message.model.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import mongoose, { Schema } from 'mongoose';
+import User from '../user/user.model';
 const Promise = require('bluebird');
 Promise.promisifyAll(mongoose);
 
@@ -13,6 +14,14 @@ var messageSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'User'
   }
+});
+
+messageSchema.post('remove', message => {
+  User.findById(message.user).exec()
+    .then(user => {
+      user.messages.pull(message);
+      user.save();
+    });
 });
 
 export default mongoose.model('Message', messageSchema);
